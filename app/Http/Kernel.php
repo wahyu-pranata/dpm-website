30f0a1989424ec39148c55632d9636dfbe399f1a
Wahyu Pranata <wahyupranata131@gmail.com>
2024-04-15 12:33:46 +0800

    Chore: delete fruitcake package

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    /**
+    /**s
      * The application's global HTTP middleware stack.
      *
      * These middleware are run during every request to your application.
@@ -16,7 +16,6 @@ class Kernel extends HttpKernel
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
